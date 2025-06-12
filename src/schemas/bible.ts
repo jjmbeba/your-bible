a67e81bda17120d97aa6efa627f9f55747d2b1f4
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-06-13 02:40:54 +0300

    tie search params with chapters as well

--- src/schemas/bible.ts
+++ src/schemas/bible.ts
@@ -3,5 +3,4 @@ import { z } from "zod";
 export const bibleSchema = z.object({
     book: z.string().min(1, { message: 'Book is required' }),
     chapter: z.string().min(1, { message: 'Chapter is required' }),
-    verse: z.string().min(1, { message: 'Verse is required' }),
 })
