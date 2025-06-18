e71530c4285d0e9d974ff9a29c312eacc2138331
Johnson Mbeba <101251043+jjmbeba@users.noreply.github.com>
2025-06-18 17:15:08 +0300

    refactor collections fetch logic on collections page

--- convex/collections.ts
+++ convex/collections.ts
@@ -2,13 +2,13 @@ import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
 
 export const get = query({
-  args: {},
-  handler: async (ctx) => {
-    // const identity = await getAuthenticatedUser(ctx);
-    
+  args: {
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
     return await ctx.db
       .query("collections")
-      // .filter((q) => q.eq(q.field("userId"), identity.subject))
+      .filter((q) => q.eq(q.field("userId"), args.userId))
       .collect();
   },
 });
